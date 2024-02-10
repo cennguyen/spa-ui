b48fef14108ed25ebac56af0c06a658fc664eea9
cen.nguyen <annp62923@gmail.com>
2024-02-10 17:31:34 +0700

    rebase release into develop

---
+++ src/app/component/BodyDynamic/screens/Product/index.js
@@ -0,0 +1,12 @@
+import React, { memo } from "react";
+
+const Product = () => {
+
+    return (
+        <React.Fragment>
+            <h1>
+                This is Product Page
+            </h1>
+        </React.Fragment>)
+}
+export default memo(Product);
